f2b05f6304aad66a218177f04b53f9553ca5af50
1z0ldyck <lucase2040@gmail.com>
2022-07-01 22:47:59 -0300

    :fire: removing init_app function and __init__ method

--- app/database/repositories/restaurant.py
+++ app/database/repositories/restaurant.py
@@ -1,14 +1,6 @@
 from models.domain.restaurant import RestaurantInterface
 
-
-def init_app(app):
-    app.repositories.restaurant = RestaurantRepository(app)
-
-
 class RestaurantRepository(RestaurantInterface):
-
-    def __init__(self, app):
-        self.app = app
 
     def create_product(self):
         ...
